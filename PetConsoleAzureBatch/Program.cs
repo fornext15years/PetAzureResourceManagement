0d334ee4865a2f52272816d78c4d963a0651b41b
kim <kim@envisionitcomau.onmicrosoft.com>
2019-08-21 17:15:03 +1000

    added testing project for azure batch service

---
+++ PetConsoleAzureBatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.Batch;
+using System;
+
+namespace PetConsoleAzureBatch
+{
+    class Program
+    {
+        private const string PoolId = "DotNetQuickstartPool";
+        private const string JobId = "DotNetQuickstartJob";
+        private const int PoolNodeCount = 2;
+        private const string PoolVMSize = "STANDARD_A1_v2";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static VirtualMachineConfiguration CreateVMConfiguration(ImageReference imageReference)
+        {
+            return new VirtualMachineConfiguration(imageReference: imageReference,nodeAgentSkuId: "batch.node.windows amd64");
+        }
+
+        private static ImageReference CreateImageReference()
+        {
+            return new ImageReference(
+                publisher: "MicrosoftWindowsServer",
+                offer: "WindowsServer",
+                sku: "2016-datacenter-smalldisk",
+                version: "latest");
+        }
+
+        private static void CreatePool(BatchClient batchClient, VirtualMachineConfiguration vmConfiguration)
+        {
+            CloudPool pool = batchClient.PoolOperations.CreatePool(poolId:PoolId, 
+                virtualMachineSize:PoolVMSize, 
+                virtualMachineConfiguration:vmConfiguration, 
+                targetDedicatedComputeNodes:PoolNodeCount);
+            pool.Commit();
+        }
+        private static void CreateJob() { }
+        private static void CreateTask() { }
+
+    }
+}
