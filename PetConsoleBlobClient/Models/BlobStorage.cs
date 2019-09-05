ea77c3b54ea99462602bd1a43e20eccf11e0d8d1
kim <kim@envisionitcomau.onmicrosoft.com>
2019-09-05 17:01:56 +1000

    Added blob storage client

---
+++ PetConsoleBlobClient/Models/BlobStorage.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConsoleBlobClient.Models
+{
+    class BlobStorage : IStorage
+    {
+        private CloudBlobClient cloudBlobClient = null;
+        public string SourceContainerName { get; set; }
+        public string DestinationContainerName { get; set; }
+
+        public BlobStorage()
+        {
+            
+        }
+        public async Task<IEnumerable<string>> GetNames()
+        {
+            List<string> names = new List<string>();
+            
+            CloudBlobContainer container = cloudBlobClient.GetContainerReference(SourceContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            BlobContinuationToken token = null;
+            BlobResultSegment segments = null;
+            do
+            {
+                segments = await container.ListBlobsSegmentedAsync(token);
+                foreach(var item in segments.Results)
+                {
+                    var blockblob = item as CloudBlockBlob;
+                    names.Add(blockblob.Name);
+                }
+
+                token = segments.ContinuationToken;
+            } while (token != null);
+
+            return names;
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Initialize");
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING", EnvironmentVariableTarget.Machine);
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+            cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task Download(string blobName, string filename)
+        {
+            Console.WriteLine("Start downloading.");
+            CloudBlobContainer container = cloudBlobClient.GetContainerReference(SourceContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            ICloudBlob blob = await container.GetBlobReferenceFromServerAsync(blobName);
+            await blob.DownloadToFileAsync(filename, FileMode.Create);
+            Console.WriteLine("End downloading.");
+            return;
+        }
+
+        public async Task Upload(string blobname, string filename)
+        {
+            Console.WriteLine("Start uploding.");
+            CloudBlobContainer container = cloudBlobClient.GetContainerReference(SourceContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(blobname);
+            await blob.UploadFromFileAsync(filename);
+            Console.WriteLine("End uploding.");
+            return;
+        }
+
+        public async Task Delete(string blobname)
+        {
+            Console.WriteLine("Start deleting.");
+            CloudBlobContainer container = cloudBlobClient.GetContainerReference(SourceContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            ICloudBlob blob = await container.GetBlobReferenceFromServerAsync(blobname);
+            await blob.DeleteIfExistsAsync();
+            Console.WriteLine("End deleting.");
+        }
+    }
+}
---
+++ PetConsoleBlobClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using PetConsoleBlobClient.Models;
+using System;
+
+namespace PetConsoleBlobClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlobStorage blobStorage = new BlobStorage();
+            blobStorage.SourceContainerName = "testcontainer";
+            blobStorage.Initialize();
+
+            //blobStorage.Download("1.jpg", "download1.jpg").GetAwaiter().GetResult();
+            //blobStorage.Upload("download1-1.jpg", "download1.jpg").GetAwaiter().GetResult();
+
+            var names = blobStorage.GetNames().GetAwaiter().GetResult();
+            if(names != null)
+            {
+                foreach(var name in names)
+                {
+                    Console.WriteLine(name);
+                    blobStorage.Delete(name).GetAwaiter().GetResult();
+                }
+            }
+
+            names = blobStorage.GetNames().GetAwaiter().GetResult();
+            if (names != null)
+            {
+                foreach (var name in names)
+                {
+                    Console.WriteLine(name);
+                    blobStorage.Delete(name).GetAwaiter().GetResult();
+                }
+            }
+            else
+            {
+                Console.WriteLine("no blob");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
